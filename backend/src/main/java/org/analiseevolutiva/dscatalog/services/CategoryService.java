928985d36ef86c2b940416b80e66c3398461d368
hailtonDavid <hailton.david@gmail.com>
2021-09-15 14:13:37 -0300

    database seeding

--- backend/src/main/java/org/analiseevolutiva/dscatalog/services/CategoryService.java
+++ backend/src/main/java/org/analiseevolutiva/dscatalog/services/CategoryService.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 import org.analiseevolutiva.dscatalog.entities.Category;
 import org.analiseevolutiva.dscatalog.respositories.CategoryRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CategoryService {
@@ -14,6 +14,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 
+	@Transactional(readOnly = true)
 	public List<Category> findALL() {
 		
 		return repository.findAll();
